d5ebe672459dfaec0c25c839e586e0848829042d
Lame Fate <119226474+lamefate@users.noreply.github.com>
2023-05-29 21:16:45 +0300

    updated permissions work

---
+++ app/src/main/java/lame/main/clear/AppPermissions.kt
@@ -0,0 +1,82 @@
+package lame.main.clear
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import android.provider.Settings
+import android.content.Context
+import android.content.Intent
+import android.os.Environment
+import android.app.Activity
+import android.os.Build
+import android.Manifest
+import android.net.Uri
+
+class AppPermissions(private val context: Context, private val activity: Activity): AppCompatActivity() {
+	var requestCodeActivity: Int = 101
+	var requestCodePermissions: Int = 100
+	
+	fun hasPermissions() {
+		val permissions = mutableListOf(
+			Manifest.permission.READ_EXTERNAL_STORAGE,
+			Manifest.permission.ACCESS_NOTIFICATION_POLICY,
+			Manifest.permission.CLEAR_APP_CACHE
+		)
+		if (Build.VERSION.SDK_INT >= 30) {
+			permissions.add(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+		} else {
+			permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+		}
+		for (permission in permissions) {
+			if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+				requestPermissions()
+				break
+			}
+		}
+	}
+	
+	fun requestPermissions() {
+		val permissions = mutableListOf(
+			Manifest.permission.READ_EXTERNAL_STORAGE,
+			Manifest.permission.ACCESS_NOTIFICATION_POLICY,
+			Manifest.permission.CLEAR_APP_CACHE
+		)
+		if (Build.VERSION.SDK_INT >= 30) {
+    		permissions.add(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+			if (!Environment.isExternalStorageManager()) {
+				try {
+        			val intent: Intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+        			intent.addCategory(Intent.CATEGORY_DEFAULT);
+					intent.data = Uri.parse("package:${context.packageName}")
+        			this.startActivityForResult(intent, requestCodeActivity);
+				} catch (e: Exception) {
+        			val intent: Intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+        			this.startActivityForResult(intent, requestCodeActivity);
+				}
+			}
+		} else {
+			permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+		}
+		ActivityCompat.requestPermissions(activity, permissions.toTypedArray(), requestCodePermissions)
+	}
+	
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    	if (requestCode == requestCodeActivity) {
+        	if (resultCode != Activity.RESULT_OK) {
+            	finishAffinity()
+        	}
+		}
+    	super.onActivityResult(requestCode, resultCode, data)
+	}
+	
+	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+		when (requestCode) {
+			requestCodePermissions -> {
+				if (grantResults.isNotEmpty() && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+					finishAffinity()
+				}
+			}
+		}
+	}
+}
--- app/src/main/java/lame/main/clear/MainActivity.kt
+++ app/src/main/java/lame/main/clear/MainActivity.kt
@@ -1,77 +1,29 @@
 package lame.main.clear
 
 import androidx.appcompat.app.AppCompatActivity
-<<<<<<< HEAD
-<<<<<<< HEAD
-import android.os.Bundle
-=======
-import android.content.Intent
-import android.widget.ImageView
-import android.Manifest
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import android.os.Bundle
-import android.net.Uri
->>>>>>> 9938106 (update)
-=======
 import androidx.core.content.ContextCompat
 import android.content.SharedPreferences
-import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import org.apache.commons.io.FileUtils
-import android.provider.Settings
 import android.widget.ImageView
 import android.content.Context
 import android.content.Intent
 import android.os.Environment
 import android.widget.Button
 import android.widget.Toast
-import android.app.Activity
 import android.os.Bundle
 import android.os.Build
 import android.Manifest
 import android.net.Uri
 import java.io.File
->>>>>>> bb05e2e (update)
 
 public class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
          setContentView(R.layout.activity_main)
-<<<<<<< HEAD
-<<<<<<< HEAD
-    }
-=======
 		 
-		 checkPermission()
-		 githubRedirection()
-    }
-	
-	fun githubRedirection() {
-		val githubLogo: ImageView = findViewById(R.id.github)
-		githubLogo.setOnClickListener {
-			val url = "https://github.com/lamefate/LameCleaner"
-			val intent = Intent(Intent.ACTION_VIEW)
-			intent.data = Uri.parse(url)
-			startActivity(intent)
-		}
-		
-	}
-	
-	fun checkPermission() {
-		if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-			ActivityCompat.requestPermissions(this,
-            	arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
-					Manifest.permission.ACCESS_NOTIFICATION_POLICY,
-					Manifest.permission.CLEAR_APP_CACHE), 100)
-		}
-	}
-	
-=======
-		 
-		 hasPermissions()
+		 AppPermissions(this,this).hasPermissions()
 		 githubHandler()
 		 
 		 val sharedPref = getSharedPreferences("app_settings", Context.MODE_PRIVATE)
@@ -112,71 +64,4 @@ public class MainActivity : AppCompatActivity() {
 			startActivity(intent)
 		}
 	}
-	
-	fun hasPermissions() {
-		val permissions = mutableListOf(
-			Manifest.permission.READ_EXTERNAL_STORAGE,
-			Manifest.permission.ACCESS_NOTIFICATION_POLICY,
-			Manifest.permission.CLEAR_APP_CACHE
-		)
-		if (Build.VERSION.SDK_INT >= 30) {
-			permissions.add(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
-		} else {
-			permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-		}
-		for (permission in permissions) {
-			if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-				requestPermissions()
-				break
-			}
-		}
-	}
-	
-	fun requestPermissions() {
-		val permissions = mutableListOf<String>()
-		permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
-		permissions.add(Manifest.permission.CLEAR_APP_CACHE)
-		permissions.add(Manifest.permission.ACCESS_NOTIFICATION_POLICY)
-		if (Build.VERSION.SDK_INT >= 30) {
-    		permissions.add(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
-			if (!Environment.isExternalStorageManager()) {
-				try {
-        			val intent: Intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-        			intent.addCategory(Intent.CATEGORY_DEFAULT);
-					intent.data = Uri.parse("package:${this.packageName}")
-        			this.startActivityForResult(intent, 101);
-				} catch (e: Exception) {
-        			val intent: Intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-        			this.startActivityForResult(intent, 101);
-				}
-			}
-		} else {
-			permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-		}
-		ActivityCompat.requestPermissions(this, permissions.toTypedArray(), 100) // 123 было
-	}
-	
-	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    	if (requestCode == 101) {
-        	if (resultCode != Activity.RESULT_OK) {
-            	finishAffinity()
-        	}
-		}
-    	super.onActivityResult(requestCode, resultCode, data)
-	}
-	
->>>>>>> bb05e2e (update)
-	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-		when (requestCode) {
-			100 -> {
-				if (grantResults.isNotEmpty() && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-					finishAffinity()
-				}
-			}
-		}
-	}
-<<<<<<< HEAD
->>>>>>> 9938106 (update)
-=======
->>>>>>> bb05e2e (update)
 }
